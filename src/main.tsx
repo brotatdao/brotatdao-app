9d76eb6ff5d8f977a3dbc3b4de8c8d103811bba1
coopertweak <127068504+coopertweak@users.noreply.github.com>
2023-11-26 16:46:34 -0600

    add polyfills

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Buffer } from 'buffer';
+import util from 'util';
+import stream from 'stream-browserify';
 import App from './App.tsx'
 import './index.css'
+
+global.Buffer = Buffer;
+global.util = util;
+global.stream = stream;
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { Buffer } from 'buffer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "./",
+  resolve: {
+    alias: {
+      // Provide correct module for 'stream' when required by dependencies
+      'stream': require.resolve('stream-browserify'),
+    },
+  },
+  define: {
+    // Provide global variables to all the scripts so that they're usable without import
+    'process.env': {},
+    'process.browser': true,
+    'Buffer': Buffer,
+  },
+  build: {
+    rollupOptions: {
+      external: ['buffer', 'util', 'stream'],
+    },
+  },
 })
