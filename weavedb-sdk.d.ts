d8ef117b42aa916d552490f0cb7cc742e6103cec
coopertweak <127068504+coopertweak@users.noreply.github.com>
2023-11-26 14:19:37 -0600

    weaveDB patch for typescript

--- weavedb-sdk.d.ts
+++ weavedb-sdk.d.ts
@@ -1,48 +1,46 @@
 declare class WeaveDB {
-    constructor(options: { contractTxId: string; nocache?: boolean });
-    init(): Promise<void>;
-    createTempAddress(address: string): Promise<any>;
-    getAddressLink(address: string): Promise<any>;
-    add(data: any, collection_name: string, user?: IUser): Promise<any>;
-    get<T>(...args: Parameters<GetFunction>): Promise<T>;
-    cget<T>(...args: Parameters<GetFunction>): Promise<
-      {
-        id: string;
-        data: T;
-        setter: string;
-        block: {
-          height: number;
-          timestamp: number;
-        };
-      }[]
-    >;
-    createTempAddress(address: string): Promise<any>;
-    delete(collection_name: string, doc_id: string, user?: IUser): Promise<any>;
-    update<T>(
-      data: T,
-      collection_name: string,
-      doc_id: string,
-      user?: IUser
-    ): Promise<any>;
-  }
-  
-  type NullableValue<T> = T | null;
+  constructor(options: { contractTxId: string; nocache?: boolean });
+  init(): Promise<void>;
+  createTempAddress(address: string): Promise<any>;
+  getAddressLink(address: string): Promise<any>;
+  add(data: any, collection_name: string, user?: IUser): Promise<any>;
+  get<T>(...args: Parameters<GetFunction>): Promise<T>;
+  cget<T>(...args: Parameters<GetFunction>): Promise<
+    {
+      id: string;
+      data: T;
+      setter: string;
+      block: {
+        height: number;
+        timestamp: number;
+      };
+    }[]
+  >;
+  createTempAddress(address: string): Promise<any>;
+  delete(collection_name: string, doc_id: string, user?: IUser): Promise<any>;
+  update<T>(
+    data: T,
+    collection_name: string,
+    doc_id: string,
+    user?: IUser
+  ): Promise<any>;
+}
 
-    type IUser = {
-        wallet: NullableValue<`0x${string}`>;
-        privateKey: NullableValue<string>;
-    };
-  
-  type CollectionQuery = string | [string, ...string[]];
-  type QueryParameter = [string, any];
-  
-  type GetFunction = {
-    (collectionName: string): void;
-    (collectionName: string, docId: string): void;
-    (
-      collectionName: string,
-      ...query: (string | QueryParameter | CollectionQuery)[]
-    ): void;
-  };
-  
-  export default WeaveDB;
+type IUser = {
+  wallet: NullableValue<`0x${string}`>;
+  privateKey: NullableValue<string>;
+};
+
+type CollectionQuery = string | [string, ...string[]];
+type QueryParameter = [string, any];
+
+type GetFunction = {
+  (collectionName: string): void;
+  (collectionName: string, docId: string): void;
+  (
+    collectionName: string,
+    ...query: (string | QueryParameter | CollectionQuery)[]
+  ): void;
+};
+
+export default WeaveDB;
