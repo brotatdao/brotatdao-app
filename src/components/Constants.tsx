35a69265c9ae7fa5ae1efcf12a1311b649ac29c3
coopertweak <127068504+coopertweak@users.noreply.github.com>
2023-12-03 13:29:10 -0600

    weave db post install version and examples

--- src/components/Constants.tsx
+++ src/components/Constants.tsx
@@ -1,4 +1,4 @@
 //Define constants used throughout app
 export const IPFS_GATEWAY='fleek.rowant.co/ipfs/';
-export const WEAVEDB_CONTRACT='8tRVME7bdwCX_kLTX29u6X6deJ4BZsHtC8S5wQLpTqw';
-export const WEAVEDB_COLLECTION='tato2';
+export const WEAVEDB_CONTRACT='qQm7rwzm4SbRcvG796I0_Iq5eA6oas0sYrrgWZ50Hko';
+export const WEAVEDB_COLLECTION='brotato0';
