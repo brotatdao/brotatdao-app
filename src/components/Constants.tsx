96266c3be72783a412e884402fc1c80424233a55
coopertweak <127068504+coopertweak@users.noreply.github.com>
2023-12-06 21:43:44 -0600

    weavedb constant

--- src/components/Constants.tsx
+++ src/components/Constants.tsx
@@ -1,4 +1,4 @@
 //Define constants used throughout app
 export const IPFS_GATEWAY='fleek.rowant.co/ipfs/';
 export const WEAVEDB_CONTRACT='qQm7rwzm4SbRcvG796I0_Iq5eA6oas0sYrrgWZ50Hko';
-export const WEAVEDB_COLLECTION='brotato0';
+export const WEAVEDB_COLLECTION='brotatdo0';
