dcbbc6baf86275abafcfcf960fa99be8f962bb88
coopertweak <127068504+coopertweak@users.noreply.github.com>
2024-01-01 19:59:41 -0600

    walletconnect web components

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,13 +1,25 @@
-// ConnectButton.tsx
-import { useWeb3Modal } from '@web3modal/wagmi/react';
+import { useState, useEffect } from 'react';
+import { useAccount } from 'wagmi';
 
 export default function ConnectButton() {
- const { open, close } = useWeb3Modal();
+ const { address, isConnecting, isDisconnected } = useAccount();
+ const [buttonLabel, setButtonLabel] = useState('Connect Wallet');
+
+ useEffect(() => {
+  if (isConnecting) {
+    setButtonLabel('Connecting...');
+  } else if (isDisconnected) {
+    setButtonLabel('Connect Wallet');
+  } else if (address) {
+    setButtonLabel('Disconnect Wallet');
+  }
+ }, [isConnecting, isDisconnected, address]);
 
  return (
-  <button onClick={(event) => {event.preventDefault(); open();}}>
-    Connect Wallet
-  </button>
+  <w3m-connect-button
+    size="md"
+    label={buttonLabel}
+    loadingLabel="Connecting..."
+  ></w3m-connect-button>
  );
 }
-
