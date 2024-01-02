8bfb118f18df77a83e90e27bdbe7276d2d33ee63
coopertweak <127068504+coopertweak@users.noreply.github.com>
2024-01-01 21:59:43 -0600

    clean up

--- types/vite-env.d.ts
+++ types/vite-env.d.ts
@@ -6,6 +6,8 @@
         VITE_FLEEK_CLIENT_ID: string;
         VITE_WALLETCONNECT_ID: string;
         VITE_OPENSEA_KEY: string;
+        VITE_INFURA: string;
+        VITE_NAMESTONE: string;
         // Add more environment variables here...
         [key: string]: string | boolean | undefined;
       };
