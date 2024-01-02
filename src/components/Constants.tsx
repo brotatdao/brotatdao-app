7cbc93968c0d7d00393d188c1af87d46a98052a8
coopertweak <127068504+coopertweak@users.noreply.github.com>
2024-01-01 21:59:43 -0600

    clean up

--- src/components/Constants.tsx
+++ src/components/Constants.tsx
@@ -1,4 +1,2 @@
 //Define constants used throughout app
 export const IPFS_GATEWAY='ipfs.brotatdao.xyz/ipfs/';
-export const WEAVEDB_CONTRACT='GKwlYVqZgp37CO4rRBH4S7iO7OptIPP-ZkoUcabkC4Q';
-export const WEAVEDB_COLLECTION='brotatdao';
