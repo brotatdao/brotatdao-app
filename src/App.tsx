adb92d941a2b88eab8aa6f7beef94e1d9e3f920f
coopertweak <127068504+coopertweak@users.noreply.github.com>
2024-01-14 17:16:25 -0600

    change to zinc colorscheme

--- src/App.tsx
+++ src/App.tsx
@@ -44,33 +44,35 @@ const App = () => {
         <nav className="bg-white">
           <div className="container mx-auto px-6 py-3 flex justify-between items-center">
             <div className="flex items-center">
-            {/* Logo Placeholder */}
-            <img src={Logo} alt="Logo" className="h-14 w-14 mr-2" />
+            <Link to="/">
+              <img src={Logo} alt="Logo" className="h-14 w-14 mr-2 cursor-pointer" />
+            </Link>
           </div>
           {/* Navigation Links and Connect Button for larger screens */}
-          <div className="hidden md:flex items-center justify-center flex-1">
-                <Link to="/" className="text-gray-800 hover:text-gray-600 px-3 py-2 rounded-md text-sm font-medium">Home</Link>
-                <Link to="/upload" className="text-gray-800 hover:text-gray-600 px-3 py-2 rounded-md text-sm font-medium">Upload</Link>
-                <Link to="/explorer" className="text-gray-800 hover:text-gray-600 px-3 py-2 rounded-md text-sm font-medium">Explorer</Link>
-              </div>
-              <div className="flex items-center">
-                <ConnectButton />
-                <div className="md:hidden">
-                  <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="text-gray-800 hover:text-gray-600 focus:outline-none focus:text-gray-600 ml-3">
-                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-                    </svg>
-                  </button>
-                </div>
-              </div>
+          <div className="hidden md:flex items-center justify-center flex-1 text-lg font-semibold">
+            <Link to="/" className="text-zinc-600 hover:text-zinc-400 px-4 py-2 rounded-md">Home</Link>
+            <Link to="/upload" className="text-zinc-600 hover:text-zinc-400 px-4 py-2 rounded-md">Upload</Link>
+            <Link to="/explorer" className="text-zinc-600 hover:text-zinc-400 px-4 py-2 rounded-md">Explorer</Link>
+          </div>
+          <div className="flex items-center">
+            <ConnectButton />
+            <div className="md:hidden">
+              <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="text-gray-800 hover:text-gray-600 focus:outline-none focus:text-gray-600 ml-3">
+                {/* Hamburger Icon */}
+                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+              </button>
             </div>
-            {isMenuOpen && (
-              <div className="md:hidden">
-                <Link to="/" className="block px-4 py-2 text-sm text-gray-800 hover:bg-gray-200">Home</Link>
-                <Link to="/upload" className="block px-4 py-2 text-sm text-gray-800 hover:bg-gray-200">Upload</Link>
-                <Link to="/explorer" className="block px-4 py-2 text-sm text-gray-800 hover:bg-gray-200">Explorer</Link>
-              </div>
-            )}
+          </div>
+        </div>
+        {isMenuOpen && (
+          <div className="md:hidden text-lg font-semibold">
+            <Link to="/" className="block px-4 py-2 text-zinc-600 hover:bg-zinc-200">Home</Link>
+            <Link to="/upload" className="block px-4 py-2 text-zinc-600 hover:bg-zinc-200">Upload</Link>
+            <Link to="/explorer" className="block px-4 py-2 text-zinc-600 hover:bg-zinc-200">Explorer</Link>
+          </div>
+        )}
           </nav>
 
       <Routes>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,9 +5,9 @@ const Home = () => {
     <div className="container mx-auto px-6 md:px-12 py-12">
         <div className="flex flex-col md:flex-row items-center justify-between">
           <div className="md:w-1/2">
-            <h1 className="text-4xl md:text-5xl xl:text-6xl font-bold leading-tight text-gray-800">Welcome to Our Platform</h1>
-            <p className="mt-2 text-gray-600">Explore, upload, and connect with the world of digital assets.</p>
-            <Link to="/upload" className="mt-4 bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-700 transition duration-300">Get Started</Link>
+            <h1 className="text-4xl md:text-5xl xl:text-6xl font-bold leading-tight text-zinc-800">Welcome to Our Platform</h1>
+            <p className="mt-2 text-zinc-600">Explore, upload, and connect with the world of digital assets.</p>
+            <Link to="/upload" className="mt-4 bg-zinc-800 text-white px-4 py-2 rounded hover:bg-zinc-700 transition duration-300">Get Started</Link>
           </div>
         </div>
       </div>
