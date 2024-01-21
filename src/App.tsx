612cd57c6046ac0b71761d65de1e4a2e49b3780b
coopertweak <127068504+coopertweak@users.noreply.github.com>
2024-01-21 13:03:23 -0600

    delete unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Upload from './pages/Upload/Upload';
 import Explorer from './pages/Explorer/Explorer';
