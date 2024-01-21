e4e9234d976c1fb1b1f925e3e604cb2e0a64766a
coopertweak <127068504+coopertweak@users.noreply.github.com>
2024-01-21 13:14:16 -0600

    profileName and bio search

--- src/pages/Explorer/Explorer.tsx
+++ src/pages/Explorer/Explorer.tsx
@@ -8,8 +8,10 @@ import { auth } from '../../components/firebaseConfig';
 
 const Explorer: React.FC = () => {
     const [profiles, setProfiles] = useState<Profile[]>([]);
-    const [viewOwnProfiles, setViewOwnProfiles] = useState(false); // State for toggle
-    const [user] = useAuthState(auth); // Get the current authenticated user
+    const [filteredProfiles, setFilteredProfiles] = useState<Profile[]>([]);
+    const [viewOwnProfiles, setViewOwnProfiles] = useState(false);
+    const [user] = useAuthState(auth);
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
 
@@ -35,22 +37,42 @@ const Explorer: React.FC = () => {
     }, [viewOwnProfiles, user]); 
 
 
-    // Toggle function
+    useEffect(() => {
+        // Filter logic
+        const lowercasedSearchTerm = searchTerm.toLowerCase();
+        const filtered = profiles.filter(profile => 
+            profile.profileName.toLowerCase().includes(lowercasedSearchTerm) ||
+            profile.bio.toLowerCase().includes(lowercasedSearchTerm)
+        );
+        setFilteredProfiles(filtered);
+    }, [searchTerm, profiles]);
+
     const toggleView = () => {
         setViewOwnProfiles(!viewOwnProfiles);
     };
 
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+    };
+
     if (!profiles.length) {
-        return <div className="text-zinc-800 font-semibold">Loading...</div>; // Or a loading spinner component
+        return <div className="text-zinc-800 font-semibold">Loading...</div>;
     }
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-zinc-50 p-4">
             <h1 className="text-3xl font-bold text-zinc-800 mb-8">Profile Explorer</h1>
+            <input
+                type="text"
+                placeholder="Search profiles..."
+                value={searchTerm}
+                onChange={handleSearchChange}
+                className="mb-4 p-2 border rounded"
+            />
             <button onClick={toggleView} className="mb-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 {viewOwnProfiles ? "View All Profiles" : "View My Profiles"}
             </button>
-            {profiles.map((profile, index) => (
+                {filteredProfiles.map((profile, index) => (
                 <div key={index} className="bg-gradient-to-br from-white to-gray-200 rounded-lg shadow-md overflow-hidden flex flex-row mb-5 w-full max-w-4xl">
                     <div className="flex-none w-1/3">
                         <img src={profile.image_url} alt="Profile Pic" className="w-full h-full object-cover rounded-l-lg" />
