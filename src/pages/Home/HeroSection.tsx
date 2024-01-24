2b2f3fac70880f146553b91a7490d71f66a07c26
coopertweak <127068504+coopertweak@users.noreply.github.com>
2024-01-23 19:56:45 -0600

    content

--- src/pages/Home/HeroSection.tsx
+++ src/pages/Home/HeroSection.tsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
         {/* Content within a centered wireframe */}
         <div className="bg-white p-8 border-2 border-gray-300 rounded-lg shadow-lg max-w-lg text-center">
           <p className="text-lg md:text-xl text-zinc-700 mb-6">sweet NFT bro.</p>
-          <h1 className="text-4xl md:text-5xl xl:text-6xl font-bold leading-tight text-zinc-800 mb-6">what it do?!</h1>
+          <h1 className="text-4xl md:text-5xl xl:text-6xl font-bold leading-tight text-zinc-800 mb-6">what it do?</h1>
           <p className="text-lg md:text-xl text-zinc-700 mb-6">your profile pic wants a profile page. </p>
           <Link to="/upload" className="inline-block bg-lime-600 text-white px-6 py-3 rounded-full hover:bg-lime-500 transition duration-300">Get Started</Link>
         </div>
