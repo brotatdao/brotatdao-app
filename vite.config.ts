415b9ad8748296b79ff70073e9610e622d9e8488
coopertweak <127068504+coopertweak@users.noreply.github.com>
2024-01-27 16:30:13 -0600

    back to esbuild

--- vite.config.ts
+++ vite.config.ts
@@ -1,56 +1,40 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 
-export default defineConfig(({ mode }) => {
-  console.log(`Running in mode: ${mode}`);
-  console.log('NODE_ENV:', process.env.NODE_ENV);
-
-  // Plugins for development
-  let plugins = [react(), nodePolyfills()];
-
-  // Plugins for production
-  if (mode === 'production') {
-    plugins = [
-      react(),
-      NodeGlobalsPolyfillPlugin({ buffer: true }),
-      NodeModulesPolyfillPlugin(),
-    ];
-  }
-
-  return {
-    plugins: plugins,
-    base: './',
-    resolve: {
-      alias: {
-        stream: 'stream-browserify',
-        buffer: 'buffer',
-        util: 'util',
+export default defineConfig({
+  plugins: [
+    react(),
+    NodeGlobalsPolyfillPlugin({
+      buffer: true,
+    }),
+    NodeModulesPolyfillPlugin(),
+  ],
+  base: "./",
+  resolve: {
+    alias: {
+      // Aliasing for browser-compatible versions
+      'stream': 'stream-browserify',
+      'buffer': 'buffer',
+      'util': 'util',
+    },
+  },
+  define: {
+    'process.env': {},
+    'process.browser': true,
+    'global.Buffer': 'Buffer.from',
+  },
+  server: {
+    proxy: {
+      '/api/public_v1': {
+        target: 'https://namestone.xyz',
+        changeOrigin: true,
+        rewrite: (path) => path,
       },
     },
-    define: {
-      'process.env': {},
-      'process.browser': true,
-      // Removed the global.Buffer definition as Buffer will be imported directly
-    },
-    server: {
-      proxy: {
-        '/api/public_v1': {
-          target: 'https://namestone.xyz',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api\/public_v1/, ''),
-        },
-      },
-    },
-    optimizeDeps: {
-      include: ['buffer'],
-    },
-    build: {
-      rollupOptions: {
-        external: mode === 'development' ? [] : ['buffer', 'util', 'stream'],
-      },
-    },
-  };
-});
+  },
+  optimizeDeps: {
+    include: ['buffer']
+  },
+})
