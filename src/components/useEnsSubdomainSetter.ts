a0c0a1a5132dcaf09fe40fc62179f354504316df
coopertweak <127068504+coopertweak@users.noreply.github.com>
2024-01-27 22:55:15 -0600

    cors

--- src/components/useEnsSubdomainSetter.ts
+++ src/components/useEnsSubdomainSetter.ts
@@ -21,7 +21,7 @@ const useEnsSubdomainSetter = () => {
 
         // check if production or development environment for proxy or direct api calls
         const apiBaseUrl = import.meta.env.MODE === 'production' 
-            ? 'https://namestone.brotatdao.xyz:8080' 
+            ? 'https://namestone.brotatdao.xyz:433' 
             : '/api/public_v1';      
 
         try {
